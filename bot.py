dceb86e2d4c9fc8cde74cffc3909216bc80a7b87
kristinius1988-hue <kristinius1988@gmail.com>
2026-05-15 10:43:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -296,13 +296,6 @@ def calculate_size(hips, waist):
     else: return "50-52"
 
 async def main():
-    # Код для Render (открывает порт, чтобы сервер не выключался)
-    app = web.Application()
-    runner = web.AppRunner(app)
-    await runner.setup()
-    port = int(os.environ.get("PORT", 8080))
-    site = web.TCPSite(runner, '0.0.0.0', port)
-    await site.start()
     async def main():
     # Код для Render
     app = web.Application()
